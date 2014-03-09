d42abf4729613c2cd29369375deadbc91320c5a7
siwangqishiq <525647740@qq.com>
2014-03-09 16:35:27 +0800

    海面素材

--- CubeLan/src/com/xinlan/cubelan/screen/GameScreen.java
+++ CubeLan/src/com/xinlan/cubelan/screen/GameScreen.java
@@ -1,4 +1,5 @@
 package com.xinlan.cubelan.screen;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
@@ -10,26 +11,26 @@ import com.badlogic.gdx.graphics.GL10;
  * 
  */
 public class GameScreen extends CubeLanScreen {
-	
+
 	public GameScreen(Game game) {
 		super(game);
 	}
-	
+
 	@Override
 	public void show() {
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
 		delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
-		
-		Gdx.gl.glClearColor(1f, 0f, 1f, 1);
+
+		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	}
 
 	@Override
 	public void hide() {
-		
+
 	}
 }// end class
